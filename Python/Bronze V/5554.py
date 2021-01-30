ae09630bb6d4a0629cba83417ffd4b327a272ae5
unknown <solar3070@naver.com>
2021-01-30 20:54:07 +0900

    bronze v

---
+++ Python/Bronze V/5554.py
@@ -0,0 +1,7 @@
+sum = 0
+
+for i in range(4):
+    sum += int(input())
+
+print(sum // 60)
+print(sum % 60)
---
+++ Python/Bronze V/8437.py
@@ -0,0 +1,5 @@
+all = int(input())
+k = int(input())
+
+print((all + k) // 2)
+print((all - k) // 2)
