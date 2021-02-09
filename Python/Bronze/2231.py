baa665de5c276e555c55d9bf8cea7a2d1fd2fc11
HyeJun Lee <solar3070@naver.com>
2021-02-10 02:31:11 +0900

    boj

--- Python/Bronze/2231.py
+++ Python/Bronze/2231.py
@@ -1,1 +1,17 @@
 N = int(input())
+
+min = N - len(str(N)) * 9
+if  len(str(min)) != len(str(min)):
+    min += 9
+
+while 0 < min < N:
+    m = list(map(int, str(min)))
+
+    if min + sum(m) == N: break
+
+    min += 1
+
+if min < 0 or min >= N:
+    print(0)
+else:
+    print(min)
